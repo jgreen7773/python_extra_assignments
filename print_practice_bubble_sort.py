953117e818c71bfa2f81ab74a02670f90e70f403
Jamie Green (Song Kran) <j.green7773@yahoo.com>
2019-09-04 08:07:02 -0700

    finished bubble sort.

--- print_practice_bubble_sort.py
+++ print_practice_bubble_sort.py
@@ -1,12 +1,21 @@
 arr = [5,3,9,2,5,7,3,1,8,3]
-print(arr)
-def swap(inserted_list):
-    for i in range(0, len(arr)):
-        if inserted_list[i] > inserted_list[i+1]:
-            inserted_list[i], inserted_list[i+1] = inserted_list[i+1], inserted_list[i]
-            print("Swapped")
-            print("\n","*"*80)
-        else:
-            print("No need to swap")
 
-swap(arr)
+def bubbleSort(arr):
+    count = 0
+    for j in range(len(arr)-1):
+        print("/n/n", "_"*50, "Iteration", j)
+        for i in range(0, len(arr))-1-j:
+            count += 1
+            # print("\n","*"*80, "\nindex ", i, "value", arr[i])
+            print("\n","*"*80, "\ncomparing", arr[i], arr[i+1])
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                print("Swapped", arr[i], arr[i+1])
+                print("array is now", arr)
+            else:
+                print("No need to swap", arr[i], arr[i+1])
+        
+        print('# of evaluations', count)
+    return arr
+
+print(bubbleSort(arr))
